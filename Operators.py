52a7e85bfda7feac128efac95bf62a79e806f0a3
Rutuja Kshirsagar <rutujakshirsagar001@gmail.com>
2026-01-05 22:03:10 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,13 @@
+#day-2
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost * tip_percent / 100
+    tax = meal_cost * tax_percent / 100
+    total_cost = meal_cost + tip + tax
+    print(round(total_cost))
+
+
+meal_cost = float(input().strip())
+tip_percent = int(input().strip())
+tax_percent = int(input().strip())
+
+solve(meal_cost, tip_percent, tax_percent)
