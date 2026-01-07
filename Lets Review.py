2902487267653867a97ae4d73ca35968298ca7ac
Rutuja Kshirsagar <rutujakshirsagar001@gmail.com>
2026-01-07 23:13:17 +0530

    Add files via upload

---
+++ Lets Review.py
@@ -0,0 +1,16 @@
+#Day 6 
+t = int(input())
+
+for _ in range(t):
+    s = input()
+    
+    even = ""
+    odd = ""
+    
+    for i in range(len(s)):
+        if i % 2 == 0:
+            even += s[i]
+        else:
+            odd += s[i]
+    
+    print(even, odd)
